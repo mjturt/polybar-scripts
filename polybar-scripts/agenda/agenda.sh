457e477669e3df60c8441bfc8090d8ca9d19aa3d
Maks Turtiainen <maks.turtiainen@lvsbrokers.com>
2020-04-14 03:04:50 +0300

    Updated agenda

--- polybar-scripts/agenda/agenda.sh
+++ polybar-scripts/agenda/agenda.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
-VIRTUALENV_ACTIVATE="$HOME/.virtualenvs/agenda/bin/activate"
+VIRTUALENV="$HOME/.virtualenvs/agenda"
+VIRTUALENV_ACTIVATE="$VIRTUALENV/bin/activate"
 GOOGLE_CREDENTIALS="$HOME/.gcredentials/credentials.json"
+DIR=$(dirname "$0")
 
-source "$VIRTUALENV_ACTIVATE"
-if [[ "$1" == "--click" ]]; then
-    "$BROWSER" "https://calendar.google.com/calendar/r/day"
+if [[ "$1" == "--install" ]]; then
+    if [[ ! -d "$VIRTUALENV" ]]; then
+        virtualenv "$VIRTUALENV"
+        source "$VIRTUALENV_ACTIVATE"
+        pip install python-bidi google-api-python-client google-auth-httplib2 google-auth-oauthlib
+        python3 "$DIR"/i3_agenda/__init__.py -c "$GOOGLE_CREDENTIALS"
+    else
+        echo "Virtualenv alredy exists"
+        exit 1
+    fi
+elif [[ "$1" == "--click" ]]; then
+    "$BROWSER" "https://calendar.google.com/calendar/r/week"
 else
-    python3 ./i3_agenda/__init__.py -c "$GOOGLE_CREDENTIALS"
+    source "$VIRTUALENV_ACTIVATE"
+    python3 "$DIR"/i3_agenda/__init__.py -c "$GOOGLE_CREDENTIALS"
 fi
